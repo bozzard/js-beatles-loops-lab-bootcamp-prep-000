3fdee1428d55a623b2395a4bb3cf3af8d9044986
Oleg Bondarchuk <bozzard@gmail.com>
2017-10-05 09:53:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function theBeatlesPlay (name, instruments) {
 }
 
 function johnLennonFacts(facts) {
+  var array = [];
   for (let i = 0; i < facts.length ; i++  ) {
     array[i] =  facts[i] + "!!!";
     console.log (array[i]);
