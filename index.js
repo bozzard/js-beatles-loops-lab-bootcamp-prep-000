d7238b76cf589af6cec4165910ab8af3f12907f3
Oleg Bondarchuk <bozzard@gmail.com>
2017-10-05 09:46:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function theBeatlesPlay (name, instruments) {
   var array = [];
-  for (let i = 0; i < length.name; i++  ) {
+  for (let i = 0; i < name.length ; i++  ) {
     array[i] =  name[i] + instruments;
     console.log (array[i]);
   }
